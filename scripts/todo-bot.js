8e7e17984f57486d053ae1d151346f9930411872
neurous <nonneurous@gmail.com>
2021-07-19 17:23:56 +0900

    list, donelistが空の場合にメッセージを表示するよう変更

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -25,9 +25,17 @@ module.exports = robot => {
     msg.send('削除しました: ' + task);
   });
   robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+    const todoList = todo.list();
+    if (todoList.length === 0) {
+      msg.send("(TODOはありません)");
+    }
+    msg.send(todoList.join('\n'));
   });
   robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
+    const todoDoneList = todo.donelist();
+    if (todoDoneList.length === 0) {
+      msg.send("(完了したTODOはありません)");
+    }
+    msg.send(todoDoneList.join('\n'));
   });
 };
