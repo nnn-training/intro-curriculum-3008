a7a063d1fe0c5799ca7e7fab69c43643c5bc35e9
pzw05mux <pzw05mux@s.okayama-u.ac.jp>
2021-09-15 16:13:57 +0900

    list、donelist が空であるときのメッセージを追加

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -8,26 +8,36 @@
 //   ボット名 donelist - 完了した TODO の一覧表示
 'use strict';
 const todo = require('todo');
-module.exports = robot => {
-  robot.respond(/add (.+)/i, msg => {
+module.exports = (robot) => {
+  robot.respond(/add (.+)/i, (msg) => {
     const task = msg.match[1].trim();
     todo.add(task);
     msg.send('追加しました: ' + task);
   });
-  robot.respond(/done (.+)/i, msg => {
+  robot.respond(/done (.+)/i, (msg) => {
     const task = msg.match[1].trim();
     todo.done(task);
     msg.send('完了にしました: ' + task);
   });
-  robot.respond(/del (.+)/i, msg => {
+  robot.respond(/del (.+)/i, (msg) => {
     const task = msg.match[1].trim();
     todo.del(task);
     msg.send('削除しました: ' + task);
   });
-  robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+  robot.respond(/list/i, (msg) => {
+    const list = todo.list();
+    if (list.length > 0) {
+      msg.send(list.join('\n'));
+    } else {
+      msg.send('（TODO はありません）');
+    }
   });
-  robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
+  robot.respond(/donelist/i, (msg) => {
+    const list = todo.donelist();
+    if (list.length > 0) {
+      msg.send(list.join('\n'));
+    } else {
+      msg.send('（完了した TODO はありません）');
+    }
   });
 };
