6da83d6ac226a57671a4bc79135a970f2bcffdcc
Kento Nakamura <kento.nakamura2009@gmail.com>
2021-09-20 13:46:55 +0900

    空白時のメッセージを設定

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -1,5 +1,5 @@
-// Description:
-//   TODO を管理できるボットです
+//Description:
+//  TODO を管理できるボットです
 // Commands:
 //   ボット名 add      - TODO を作成
 //   ボット名 done     - TODO を完了にする
@@ -9,25 +9,35 @@
 'use strict';
 const todo = require('todo');
 module.exports = robot => {
-  robot.respond(/add (.+)/i, msg => {
-    const task = msg.match[1].trim();
-    todo.add(task);
-    msg.send('追加しました: ' + task);
-  });
-  robot.respond(/done (.+)/i, msg => {
-    const task = msg.match[1].trim();
-    todo.done(task);
-    msg.send('完了にしました: ' + task);
-  });
-  robot.respond(/del (.+)/i, msg => {
-    const task = msg.match[1].trim();
-    todo.del(task);
-    msg.send('削除しました: ' + task);
-  });
-  robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
-  });
-  robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
-  });
+    robot.respond(/add (.+)/i, msg => {
+        const task = msg.match[1].trim();
+        todo.add(task);
+        msg.send('追加しました: ' + task);
+    });
+    robot.respond(/done (.+)/i, msg => {
+        const task = msg.match[1].trim();
+        todo.done(task);
+        msg.send('完了にしました: ' + task);
+    });
+    robot.respond(/del (.+)/i, msg => {
+        const task = msg.match[1].trim();
+        todo.del(task);
+        msg.send('削除しました: ' + task);
+    });
+    robot.respond(/list/i, msg => {
+        const list = todo.list();
+        if (list.length === 0) {
+            msg.send('TODOはありません');
+        } else {
+            msg.send(list().join('\n'));
+        }
+    });
+    robot.respond(/donelist/i, msg => {
+        const donelist = todo.donelist();
+        if (donelist.length === 0) {
+            msg.send('完了したTODOはありません');
+        } else {
+            msg.send(donelist.join('\n'));
+        }
+    });
 };
