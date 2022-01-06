b99c08b8101721d2677ff68096b6829053fa101d
Kota Shimizu <s.kouta0723@outlook.jp>
2022-01-06 14:51:44 +0900

    list が空のときの処理を追加

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -25,9 +25,17 @@ module.exports = robot => {
     msg.send('削除しました: ' + task);
   });
   robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+    if(todo.list().length === 0){
+      msg.send('(TODOはありません)');
+    }else{
+      msg.send(todo.list().join('\n'));
+    }
   });
   robot.respond(/donelist/i, msg => {
+    if(todo.donelist().length === 0){
+      msg.send('(完了したTODOはありません)');
+    }else{
     msg.send(todo.donelist().join('\n'));
+    }
   });
 };
