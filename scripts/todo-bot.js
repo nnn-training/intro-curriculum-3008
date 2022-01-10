6b86d308893852afd90d106909cb0c3ee6d5393c
ShinjiSawada <0ef7mz10388321c@au.com>
2022-01-10 20:55:45 +0900

    ボットとインターフェースの連携の練習問題完了

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -25,9 +25,22 @@ module.exports = robot => {
     msg.send('削除しました: ' + task);
   });
   robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+    const list = todo.list();
+    if (list.length === 0) {
+      msg.send('(TODOはありません)');
+    } else {
+      msg.send(todo.list().join('\n'));
+    }
   });
   robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
+    const donelist = todo.donelist();
+    if (donelist.length === 0) {
+      msg.send('(完了したTODOはありません)');
+      if (donelist.length === 0) {
+        msg.send('(完了したTODOはありません)');
+      } else {
+        msg.send(todo.donelist().join('\n'));
+      }
+    }
   });
 };
