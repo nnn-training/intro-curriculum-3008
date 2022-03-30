23278cb04e0c8801ae988668dd166a009668f36f
ValleySword <kenshin.study.randint@gmail.com>
2022-03-30 14:19:24 +0900

    リストが空の際の発言の追加

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -25,9 +25,17 @@ module.exports = robot => {
     msg.send('削除しました: ' + task);
   });
   robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+    if (todo.list.length === 0) {
+      msg.send('(TODO はありません)');
+    } else {
+      msg.send(todo.list().join('\n'));
+    }
   });
   robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
+    if (todo.donelist.length === 0) {
+      msg.send('(完了した TODO はありません');
+    } else {
+      msg.send(todo.donelist().join('\n'));
+    }
   });
 };
