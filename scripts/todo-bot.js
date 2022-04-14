69a5996c16480033bbdd25bb98c74c86b76ee9db
Takayuki Ogawa <sora758@me.com>
2022-04-14 21:58:43 +0900

    botの発言を訂正

--- scripts/todo-bot.js
+++ scripts/todo-bot.js
@@ -25,9 +25,19 @@ module.exports = robot => {
     msg.send('削除しました: ' + task);
   });
   robot.respond(/list/i, msg => {
-    msg.send(todo.list().join('\n'));
+    const list = todo.list();
+    if (list.length === 0) {
+      msg.send('TODOはありません');
+    } else {
+      msg.send(todo.list().join('\n'));
+    }
   });
   robot.respond(/donelist/i, msg => {
-    msg.send(todo.donelist().join('\n'));
+    const list = todo.list();
+    if (list.length === 0) {
+      msg.send('完了した TODO はありません');
+    } else {
+      msg.send(todo.donelist().join('\n'));    
+    }
   });
 };
