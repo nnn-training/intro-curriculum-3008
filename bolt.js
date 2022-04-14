a4ac6ceab6748c4c1cfd5f6ba862546de868daa2
YURI <taro0koma@gmail.com>
2022-04-14 15:33:12 +0900

    ボットメッセージの修正:空リスト対応を追加実装

--- bolt.js
+++ bolt.js
@@ -13,36 +13,52 @@ dotenv.config();
 const todo = require('todo');
 
 const app = new bolt.App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
-  logLevel: 'debug'
+    token: process.env.SLACK_BOT_TOKEN,
+    appToken: process.env.SLACK_APP_TOKEN,
+    socketMode: true,
+    logLevel: 'debug'
 });
 
-app.message(/add (.+)/i, ({context, say}) => {
-  const task = context.matches[1].trim();
-  todo.add(task);
-  say(`追加しました: ${task}`);
+app.message(/add (.+)/i, ({ context, say }) => {
+    const task = context.matches[1].trim();
+    todo.add(task);
+    say(`追加しました: ${task}`);
 });
 
-app.message(/done (.+)/i, ({context, say}) => {
-  const task = context.matches[1].trim();
-  todo.done(task);
-  say(`完了にしました: ${task}`);
-});
- 
-app.message(/del (.+)/i, ({context, say}) => {
-  const task = context.matches[1].trim();
-  todo.del(task);
-  say(`削除しました: ${task}`);
+app.message(/done (.+)/i, ({ context, say }) => {
+    const task = context.matches[1].trim();
+    if (todo.list().indexOf(task) > -1) {
+        todo.done(task);
+        say(`完了にしました: ${task}`);
+    } else {
+        say('（そのタスクは、TODOリスト内にありませんでした😅）');
+    }
 });
 
-app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+app.message(/del (.+)/i, ({ context, say }) => {
+    const task = context.matches[1].trim();
+    todo.del(task);
+    say(`削除しました: ${task}`);
 });
 
-app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
+app.message(/^list/i, ({ context, say }) => {
+    // say(todo.list().join('\n'));
+    const list = todo.list();
+    if (list.length === 0) {
+        say('（ TODO はありませんでした👌 ）')
+    } else {
+        say(list.join('\n'));
+    }
+});
+
+app.message(/donelist/i, ({ context, say }) => {
+    // say(todo.donelist().join('\n'));
+    const donelist = todo.donelist();
+    if (donelist.length === 0) {
+        say('（ 完了したTODOはありませんでした😲 ）');
+    } else {
+        say(donelist.join('\n'));
+    }
 });
 
 app.start();
