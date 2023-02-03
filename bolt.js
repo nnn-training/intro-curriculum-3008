1b3a38419ef45755fc0823221aec7f73fcc8f49f
Yuu YABU <wongty0220@gmail.com>
2023-02-03 14:11:44 +0900

    list、donelist が空であるときの発言を修正

--- bolt.js
+++ bolt.js
@@ -6,43 +6,53 @@
 //   ボット名 del      - TODO を消す
 //   ボット名 list     - TODO の一覧表示
 //   ボット名 donelist - 完了した TODO の一覧表示
-'use strict';
-const bolt = require('@slack/bolt');
-const dotenv = require('dotenv');
+"use strict";
+const bolt = require("@slack/bolt");
+const dotenv = require("dotenv");
 dotenv.config();
-const todo = require('todo');
+const todo = require("todo");
 
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
-  logLevel: 'debug'
+  logLevel: "debug",
 });
 
-app.message(/add (.+)/i, ({context, say}) => {
+app.message(/add (.+)/i, ({ context, say }) => {
   const taskName = context.matches[1].trim();
   todo.add(taskName);
   say(`追加しました: ${taskName}`);
 });
 
-app.message(/done (.+)/i, ({context, say}) => {
+app.message(/done (.+)/i, ({ context, say }) => {
   const taskName = context.matches[1].trim();
   todo.done(taskName);
   say(`完了にしました: ${taskName}`);
 });
- 
-app.message(/del (.+)/i, ({context, say}) => {
+
+app.message(/del (.+)/i, ({ context, say }) => {
   const taskName = context.matches[1].trim();
   todo.del(taskName);
   say(`削除しました: ${taskName}`);
 });
 
-app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+app.message(/^list/i, ({ context, say }) => {
+  const list = todo.list();
+  if (list.length !== 0) {
+    say(list.join("\n"));
+  } else {
+    say("（TODO はありません）");
+  }
 });
 
-app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
+app.message(/donelist/i, ({ context, say }) => {
+  const donelist = todo.donelist();
+  if (donelist.length !== 0) {
+    say(donelist.join("\n"));
+  } else {
+    say("（完了した TODO はありません）");
+  }
 });
 
 app.start();
