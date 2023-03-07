a1bbe85fabe404877f3a109006cfdcd667079124
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-03-07 14:17:41 +0900

    listとdonelistが空の場合にエラーとなる不具合を修正

--- bolt.js
+++ bolt.js
@@ -38,11 +38,19 @@ app.message(/del (.+)/i, ({context, say}) => {
 });
 
 app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+  if (todo.list().length === 0) {
+    say("（TODOはありません）");
+  } else {
+    say(todo.list().join('\n'));
+  }
 });
 
 app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
+  if (todo.donelist().length === 0) {
+    say("（完了したTODOはありません）");
+  } else {
+    say(todo.donelist().join('\n'));
+  }
 });
 
 app.start();
