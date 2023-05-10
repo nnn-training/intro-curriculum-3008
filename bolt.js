357d0ec78f19377c5dda2be39b5dadcca7bce95f
dw-sokuno <131229199+dw-sokuno@users.noreply.github.com>
2023-05-10 13:30:07 +0900

    list, donelistが空のときの応答を追加

--- bolt.js
+++ bolt.js
@@ -38,11 +38,22 @@ app.message(/del (.+)/i, ({context, say}) => {
 });
 
 app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+  const list = todo.list();
+  if (list.length === 0){
+    say("TODOはありません");
+  } else {
+    say(list.join('\n'));
+  }
+
 });
 
 app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
+  const list = todo.donelist();
+  if (list.length === 0){
+    say("TODOはありません");
+  } else {
+    say(list.join('\n'));
+  }
 });
 
 app.start();
