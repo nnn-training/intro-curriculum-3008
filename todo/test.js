603e2f928868654b22c325e5f91fa43b0b4d879b
dorimiamn <shunsuke_midorikawa@dwango.co.jp>
2024-03-25 19:19:29 +0900

    node:test を使用した記述に変更

--- todo/test.js
+++ todo/test.js
@@ -1,21 +1,23 @@
 'use strict';
 const todo = require('./index.js');
 const assert = require('node:assert');
+const test = require('node:test');
 
-// add と list のテスト
-todo.add('ノートを買う');
-todo.add('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+test('addとlistのテスト', () => {
+  todo.add('ノートを買う');
+  todo.add('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う', '鉛筆を買う']);
+});
 
-// done と donelist のテスト
-todo.done('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), ['ノートを買う']);
-assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
+test('doneとdonelistのテスト', () => {
+  todo.done('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), ['ノートを買う']);
+  assert.deepStrictEqual(todo.donelist(), ['鉛筆を買う']);
+});
 
-// del のテスト
-todo.del('ノートを買う');
-todo.del('鉛筆を買う');
-assert.deepStrictEqual(todo.list(), []);
-assert.deepStrictEqual(todo.donelist(), []);
-
-console.log('テストが正常に完了しました');
+test('delのテスト', () => {
+  todo.del('ノートを買う');
+  todo.del('鉛筆を買う');
+  assert.deepStrictEqual(todo.list(), []);
+  assert.deepStrictEqual(todo.donelist(), []);
+});
