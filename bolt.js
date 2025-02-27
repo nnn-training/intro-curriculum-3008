6cfbc40d16e017c5e8bc9bbc0e5a6483cae9191d
chi-momo1358 <iwasachi0618@gmail.com>
2025-02-27 14:44:50 +0900

    タスクがない場合の修正

--- bolt.js
+++ bolt.js
@@ -1,48 +1,56 @@
 // Description:
 //   TODO を管理できるボットです
 // Commands:
-//   ボット名 add      - TODO を作成
+//   ボット名 add      - TODO を作成する
 //   ボット名 done     - TODO を完了にする
-//   ボット名 del      - TODO を消す
-//   ボット名 list     - TODO の一覧表示
-//   ボット名 donelist - 完了した TODO の一覧表示
-'use strict';
-const bolt = require('@slack/bolt');
-const dotenv = require('dotenv');
-dotenv.config();
-const todo = require('todo');
+//   ボット名 del      - TODO を削除する
+//   ボット名 list     - TODO の一覧を表示する
+//   ボット名 donelist - 完了した TODO の一覧を表示する
+'use strict'
+const bolt = require('@slack/bolt')
+const todo = require('todo')
 
 const app = new bolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
   socketMode: true,
   logLevel: 'debug'
-});
+})
 
-app.message(/add (.+)/i, ({context, say}) => {
-  const taskName = context.matches[1].trim();
-  todo.add(taskName);
-  say(`追加しました: ${taskName}`);
-});
+app.message(/add (.+)/i, ({context, say}) =>{
+  const taskName = context.matches[1].trim()
+  todo.add(taskName)
+  say(`追加しました: ${taskName}`)
+})
 
-app.message(/done (.+)/i, ({context, say}) => {
-  const taskName = context.matches[1].trim();
-  todo.done(taskName);
-  say(`完了にしました: ${taskName}`);
-});
- 
-app.message(/del (.+)/i, ({context, say}) => {
-  const taskName = context.matches[1].trim();
-  todo.del(taskName);
-  say(`削除しました: ${taskName}`);
-});
+app.message(/done (.+)/i, ({context, say}) =>{
+  const taskName = context.matches[1].trim()
+  todo.done(taskName)
+  say(`完了にしました: ${taskName}`)
+})
+
+app.message(/del (.+)/i, ({context, say}) =>{
+  const taskName = context.matches[1].trim()
+  todo.del(taskName)
+  say(`削除しました: ${taskName}`)
+})
 
 app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
-});
+  const list = todo.list()
+  if (list.length === 0) {
+    say('(TODO はありません)')
+  } else {
+    say(list.join('\n'))
+  }
+})
 
 app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
-});
+  const donelist = todo.donelist()
+  if (donelist.length === 0) {
+    say('(完了した TODO はありません)')
+  } else {
+    say(donelist.join('\n'))
+  }
+})
 
-app.start();
+app.start()
