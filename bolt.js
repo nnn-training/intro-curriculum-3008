0522a4e6b4e30ac57178a499a0948794cf2f1ecd
yukie-cyber <251zs03938@student.zen.ac.jp>
2026-02-08 22:06:34 +0900

    タスクがない時は「ありません」的なメッセージを出す

--- bolt.js
+++ bolt.js
@@ -38,7 +38,12 @@ app.message(/del (.+)/i, ({context, say}) => {
 });
 
 app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+  let list = todo.list();
+  if(list.length === 0) {
+    say(' (TODO はありません) ');
+  } else {
+    say(list.join('\n'));
+  }
 });
 
 app.message(/donelist/i, ({context, say}) => {
