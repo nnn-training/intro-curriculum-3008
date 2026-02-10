563f9fe5192ec3f123c3a463a3c402cabb7fbe2e
Toyoshima Rei <358rayray@gmail.com>
2026-02-11 03:43:40 +0900

    タスクが何もない時に動作が停止しないように

--- bolt.js
+++ bolt.js
@@ -38,11 +38,21 @@ app.message(/del (.+)/i, ({context, say}) => {
 });
 
 app.message(/^list/i, ({context, say}) => {
-  say(todo.list().join('\n'));
+  const list = todo.list()
+  if (list.length >0) {
+    say(list.join('\n'));
+  } else {
+    say(`何かを始めましょう。`);
+  }
 });
 
 app.message(/donelist/i, ({context, say}) => {
-  say(todo.donelist().join('\n'));
+  const list = todo.donelist()
+  if (list.length >0) {
+    say(list.join('\n'));
+  } else {
+    say(`何かをやってみましょう。`);
+  }
 });
 
 app.start();
